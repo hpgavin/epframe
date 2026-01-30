3c0835050de3033d3231ba9e241df6e7cac6716e
Henri P Gavin <henri.gavin@duke.edu>
2026-01-30 03:20:09 -0500

    adjust she-bang

--- src/epframe.py
+++ src/epframe.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 """
 ELASTIC PLASTIC ANALYSIS OF A PLANE FRAME
 Translated from FORTRAN epframe.f by Hacksoo Lee, 1986
--- src/epframe_viz.py
+++ src/epframe_viz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 """
 EPFRAME Visualization Tools
 Plots frame geometry, deformed shape, and force diagrams
